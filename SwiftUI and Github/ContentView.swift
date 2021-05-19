83825f2a7c06e08c73be9c7ea69210516ed076b6
Javier_Gonzalez <61952186+JavierGonzalez1@users.noreply.github.com>
2021-05-19 15:34:40 -0700

    This is a commit message 1_2_3

--- SwiftUI and Github/ContentView.swift
+++ SwiftUI and Github/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        Text("Github")
+        
+//        @State private var sliderValue = 0.0
+        
+        ProgressView(value: 0.5)
+        
+        ProgressView()
+        
+//        Slider(value: $sliderValue)
     }
 }
