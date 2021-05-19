40dc446e9eeeb3195150f5f4f48c866d81beda9f
Javier_Gonzalez <61952186+JavierGonzalez1@users.noreply.github.com>
2021-05-19 14:32:27 -0700

    Initial Commit

---
+++ SwiftUI and Github/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI and Github
+//
+//  Created by Javier Gonzalez on 5/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI and Github/SwiftUI_and_GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_and_GithubApp.swift
+//  SwiftUI and Github
+//
+//  Created by Javier Gonzalez on 5/19/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_and_GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
